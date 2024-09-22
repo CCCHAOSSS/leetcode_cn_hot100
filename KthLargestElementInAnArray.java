3a0b658b00cbdeb2fbaaee75ea699ba51c583b5d
limei <lmliang11@foxmail.com>
2024-09-22 17:17:28 +0800

    数组中第k大的元素

---
+++ KthLargestElementInAnArray.java
@@ -0,0 +1,28 @@
+import java.util.PriorityQueue;
+
+public class KthLargestElementInAnArray {
+
+    public static void main(String[] args) {
+        //...
+    }
+
+    /**
+     * 数组中第k个最大元素
+     * */
+    public static int findKthLargest(int[] nums, int k) {
+        int res = 0;
+        // 用小顶堆
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (pq.size() < k) {
+                pq.add(nums[i]);
+            }else {
+                if (pq.peek() < nums[i]) {
+                    pq.poll();
+                    pq.add(nums[i]);
+                }
+            }
+        }
+        return pq.peek();
+    }
+}
