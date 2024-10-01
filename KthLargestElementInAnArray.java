94380cbd69cfd44d5aa2c140561489ba24e1634f
limei <lmliang11@foxmail.com>
2024-10-01 10:45:41 +0800

    构建堆和堆调整

--- KthLargestElementInAnArray.java
+++ KthLargestElementInAnArray.java
@@ -3,7 +3,8 @@ import java.util.PriorityQueue;
 public class KthLargestElementInAnArray {
 
     public static void main(String[] args) {
-        //...
+        int[] nums = new int[]{1, 2, 6, 8, 9, 10, 3, 19, 4, 33, 20};
+        System.out.println(findKthLargest(nums, 3));
     }
 
     /**
@@ -12,18 +13,53 @@ public class KthLargestElementInAnArray {
      * */
     public static int findKthLargest(int[] nums, int k) {
         int res = 0;
-        // 用小顶堆
+
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int i = 0; i < nums.length; i++) {
+        for (int num : nums) {
             if (pq.size() < k) {
-                pq.add(nums[i]);
-            }else {
-                if (pq.peek() < nums[i]) {
+                pq.offer(num);
+            } else {
+                if(pq.peek() < num){
                     pq.poll();
-                    pq.add(nums[i]);
+                    pq.offer(num);
                 }
             }
         }
         return pq.peek();
+    }
+
+    /**
+     * 建堆
+     * */
+    public static void buildHeap(int[] nums, int heapSize) {
+        // 从非叶子节点开始
+        for (int i = heapSize / 2 - 1; i >= 0; i--) {
+            adjustHeap(nums, i, heapSize);
+        }
+
+    }
+
+    /**
+     * d堆调整
+     * */
+    public static void adjustHeap(int[] nums, int root, int heapSize) {
+        int l = root*2 + 1, r = root*2 + 2;
+        int least = root;
+        if (l < heapSize && nums[l] < nums[least]) {
+            least = l;
+        }
+        if (r < heapSize && nums[r] < nums[least]) {
+            least = r;
+        }
+        if (least != root) {
+            swap(nums, root, least);
+        }
+        adjustHeap(nums, least, heapSize);
+    }
+
+    public static void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 }
---
+++ MoveZeroes.java
@@ -0,0 +1,12 @@
+public class MoveZeroes {
+
+    public static void main(String[] args) {}
+
+    /**
+     * 移动零
+     * */
+    public static void moveZeroes(int[] nums) {
+        //快慢指针
+
+    }
+}
