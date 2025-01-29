f007e0e95b66295043e071b290c51f05f3ba3270
limei <lmliang11@foxmail.com>
2025-01-29 13:41:00 +0800

    二叉树的最近公共祖先

--- LowestCommonAncestorofaBinaryTree.java
+++ LowestCommonAncestorofaBinaryTree.java
@@ -1,4 +1,45 @@
-package PACKAGE_NAME;
+import javax.swing.tree.TreeNode;
 
 public class LowestCommonAncestorofaBinaryTree {
+
+    public static void main(String[] args) {
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(){}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * 二叉树的最近公共祖先
+     * */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+
+        if (root.val == p.val || root.val == q.val) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left != null) {
+            return left;
+        }else{
+            return right;
+        }
+
+    }
 }
